60bc00b812374d92b55c8497f4355c7837e3912b
xzGithu <18601135464@163.com>
2017-04-24 17:49:57 +0800

    test

--- testdj/testd/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from testd.models import *
-
-admin.site.register(MySite)
-# Register your models here.
--- testdj/testd/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class Mybook(forms.Form):
-    name = forms.CharField()
-    author = forms.CharField()
-    date = forms.CharField()
